eeaa5b74fe538027ce3b9d12bd10ba9c0a34001f
ElbioLezchik <elbiolezchik@gmail.com>
2022-06-03 19:42:01 -0300

    Mi primer commit

--- familia/miembros/models.py
+++ familia/miembros/models.py
@@ -1,24 +1,25 @@
-import email
+
 from django.db import models
 
 
 # Create your models here.
 class Mascota(models.Model):
     """Model representa a una mascota."""
-    name = models.CharField(max_length=200, help_text='Ingrese el nombre de la mascota')
+    nombre = models.CharField(max_length=200, help_text='Ingrese el nombre de la mascota')
     tipo_de_mascota = models.CharField(max_length=200, help_text='Ingrese el tipo de mascota. P. ej: Gato, Perro')
     raza = models.CharField(max_length=200, help_text='Ingrese la raza de la mascota')
 
     def __str__(self):
-        """String for representing the Model object."""
-        return self.name
+        
+        return self.nombre
 
 
 class Hobby(models.Model):
-    """Model que representa a un hobby."""
+    
     actividad = models.CharField(max_length=200, help_text='Ingrese el hobby')
+    
     def __str__(self):
-        """String for representing the Model object."""
+    
         return self.actividad
 
 
@@ -28,14 +29,16 @@ class Persona(models.Model):
     nombre = models.CharField(max_length=100)
     fecha_de_nacimiento = models.DateField(null=True, blank=True)
     direccion = models.CharField(max_length=100)
-    telefono = models.IntegerField()
-    email= models.CharField(max_length=100)
-    mascota = models.ManyToManyField('Mascota')
-    hobby = models.ManyToManyField('Hobby')
-
+    telefono = models.IntegerField(null=True, blank=True)
+    correo_electronico = models.EmailField(null=True, blank=True)
+    mascota = models.ManyToManyField(Mascota, null=True, blank=True, help_text='Seleccione la mascota de este familiar')
+    hobby = models.ManyToManyField(Hobby, null=True, blank=True, help_text='Seleccione el hobby de este familiar')
     fecha_de_fallecimiento = models.DateField(null=True, blank=True)
 
-    class Meta:
-        ordering = ['apellido', 'nombre']
+    
+
+    def __str__(self):
+        
+        return f'{self.apellido}, {self.nombre}'
 
 
